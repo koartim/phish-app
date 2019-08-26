fa0e2e096c437950cd1aa5afefbe7fa970b09a14
Tim Koar <koartimothy@gmail.com>
2019-08-26 13:30:19 -0400

    changed data types for show table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+response = RestClient.get("https://api.phish.net/v3/shows/links?apikey=07E4EA1561E2A5D45A58")
+  data JSON.parse(response)
+    data.each do |show|
+      Show.create!(artist: show["response"]["data"]["artist"])
+    end
